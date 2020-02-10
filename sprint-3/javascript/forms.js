edfcaa59eec220f0caf22eb0706342a5225e5444
Jason <jason.lo.kc@gmail.com>
2020-02-09 23:19:58 -0800

    sprint 3 done

---
+++ sprint-3/javascript/forms.js
@@ -0,0 +1,181 @@
+// let form = document.getElementById("comment__form");
+
+// form.addEventListener("submit", event => {
+//   event.preventDefault();
+
+//   let usercomment = {};
+//   usercomment.name = event.target.name.value;
+//   usercomment.comment = event.target.comment.value;
+
+//   let userCommentSection = document.querySelector(".comment");
+//   let commentDiv = document.createElement("div");
+//   commentDiv.classList.add("comment__output");
+
+//   let commentOutputUpper = document.createElement("div");
+//   commentOutputUpper.classList.add("comment__upper");
+
+//   let picOutput = document.createElement("div");
+//   picOutput.classList.add("comment__pic");
+
+//   let nameOutput = document.createElement("h3");
+//   nameOutput.classList.add("comment__name");
+//   nameOutput.innerText = usercomment.name;
+
+//   let dateOutput = document.createElement("h3");
+//   dateOutput.classList.add("comment__date");
+//   let d = new Date();
+//   let commentDate = d.getUTCDate();
+//   let commentMonth = d.getUTCMonth();
+//   let commentYear = d.getUTCFullYear();
+//   dateOutput.innerHTML = `${commentDate}/ ${commentMonth}/ ${commentYear}`;
+
+//   let commentOutput = document.createElement("p");
+//   commentOutput.classList.add("comment__comment");
+//   commentOutput.innerText = usercomment.comment;
+
+//   commentOutputUpper.appendChild(picOutput);
+//   commentOutputUpper.appendChild(nameOutput);
+//   commentOutputUpper.appendChild(dateOutput);
+//   commentDiv.appendChild(commentOutputUpper);
+//   commentDiv.appendChild(commentOutput);
+//   userCommentSection.appendChild(commentDiv);
+// });
+
+// let defaultComment = [
+//   {
+//     name: "Micheal Lyons",
+//     date: "12/18/2018",
+//     comment:
+//       "They BLEW the ROOF off at their last show, once everyone started ﬁguring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+//   },
+//   {
+//     name: "Gary Wong",
+//     date: "12/12/2018",
+//     comment:
+//       "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!"
+//   },
+//   {
+//     name: "Theodore Duncan",
+//     date: "11/15/2018",
+//     comment:
+//       "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s deﬁnitely my favorite ever!"
+//   }
+// ];
+
+// function generateDefaultComment(defaultCommentobj) {
+//   let comments = document.querySelector(".defaultcomment");
+
+//   let commentSection = document.createElement("div");
+//   commentSection.classList.add("defaultcomment__section");
+
+//   let commentUpper = document.createElement("div");
+//   commentUpper.classList.add("defaultcomment__upper");
+
+//   let commenterPic = document.createElement("div");
+//   commenterPic.classList.add("defaultcomment__pic");
+
+//   let commenterName = document.createElement("h3");
+//   commenterName.classList.add("defaultcomment__name");
+//   commenterName.innerText = defaultCommentobj.name;
+
+//   let commenterDate = document.createElement("h3");
+//   commenterDate.classList.add("defaultcomment__date");
+//   commenterDate.innerText = defaultCommentobj.date;
+
+//   let commenterComment = document.createElement("p");
+//   commenterComment.classList.add("defaultcomment__comment");
+//   commenterComment.innerText = defaultCommentobj.comment;
+
+//   commentUpper.appendChild(commenterPic);
+//   commentUpper.appendChild(commenterName);
+//   commentUpper.appendChild(commenterDate);
+//   commentSection.appendChild(commentUpper);
+//   commentSection.appendChild(commenterComment);
+//   comments.appendChild(commentSection);
+// }
+
+// for (let x = 0; x < defaultComment.length; x++) {
+//   generateDefaultComment(defaultComment[x]);
+// }
+
+// Get comments from API
+
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/comments?api_key=3199f46c-ce0e-4840-80c4-0781158980ed"
+  )
+  .then(response => {
+    let commentsFromApi = response.data;
+    console.log(commentsFromApi);
+    console.log(commentsFromApi.reverse());
+    for (let x = 0; x < commentsFromApi.length; x++) {
+      let theArray = [x];
+      generateDefaultComment(commentsFromApi[x]);
+    }
+  })
+  .catch(function(error) {
+    // handle error
+    console.log(error);
+  });
+function generateDefaultComment(comment) {
+  let comments = document.querySelector(".defaultcomment");
+
+  let commentSection = document.createElement("div");
+  commentSection.classList.add("defaultcomment__section");
+
+  let commentUpper = document.createElement("div");
+  commentUpper.classList.add("defaultcomment__upper");
+
+  let commenterPic = document.createElement("div");
+  commenterPic.classList.add("defaultcomment__pic");
+
+  let commenterName = document.createElement("h3");
+  commenterName.classList.add("defaultcomment__name");
+  commenterName.innerText = comment.name;
+
+  let commenterDate = document.createElement("h3");
+  commenterDate.classList.add("defaultcomment__date");
+  let g = comment.timestamp;
+  let d = new Date(g);
+  let commentDate = d.getUTCDate();
+  let commentMonth = d.getUTCMonth();
+  let commentYear = d.getUTCFullYear();
+  commenterDate.innerHTML = `${commentDate}/ ${commentMonth}/ ${commentYear}`;
+
+  let commenterComment = document.createElement("p");
+  commenterComment.classList.add("defaultcomment__comment");
+  commenterComment.innerText = comment.comment;
+
+  commentUpper.appendChild(commenterPic);
+  commentUpper.appendChild(commenterName);
+  commentUpper.appendChild(commenterDate);
+  commentSection.appendChild(commentUpper);
+  commentSection.appendChild(commenterComment);
+  comments.appendChild(commentSection);
+}
+
+//POST comments on API
+
+let form = document.getElementById("comment__form");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  let usercomment = {};
+  usercomment.name = event.target.name.value;
+  usercomment.comment = event.target.comment.value;
+
+  axios
+    .post(
+      "https://project-1-api.herokuapp.com/comments?api_key=3199f46c-ce0e-4840-80c4-0781158980ed",
+      usercomment
+    )
+    .then(function(response) {
+      console.log("It went through");
+      //reload the comments calling the get comment function
+      // wrap the get comment inside a function
+    })
+    .catch(function(error) {
+      console.log("stupid");
+    });
+});
---
+++ sprint-3/javascript/showpage.js
@@ -0,0 +1,126 @@
+document.querySelector(".musichero__albumname").innerText =
+  "Music To Be Murdered By";
+document.querySelector(".musichero__songname").innerText = "Lock It Up";
+
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/showdates?api_key=198fdc7d-5eae-467e-a297-33259c534e28"
+  )
+  .then(response => {
+    let showsFromApi = response.data;
+    for (let i = 0; i < showsFromApi.length; i++) {
+      generateShowList(showsFromApi[i]);
+    }
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+function generateShowList(showsInfo) {
+  let shows = document.querySelector(".shows");
+
+  let showsEl = document.createElement("div");
+  showsEl.classList.add("shows__content");
+
+  let h3Date = document.createElement("h3");
+  h3Date.classList.add("shows__content--date");
+  h3Date.innerText = "DATE";
+  let pDate = document.createElement("p");
+  pDate.classList.add("shows__content--date--context");
+  pDate.innerText = showsInfo.date;
+  let h3Venue = document.createElement("h3");
+  h3Venue.classList.add("shows__content--venue");
+  h3Venue.innerText = "VENUE";
+  let pVenue = document.createElement("p");
+  pVenue.classList.add("shows__content--venue--context");
+  pVenue.innerText = showsInfo.place;
+  let h3Location = document.createElement("h3");
+  h3Location.classList.add("shows__content--location");
+  h3Location.innerText = "LOCATION";
+  let pLocation = document.createElement("p");
+  pLocation.classList.add("shows__content--location--context");
+  pLocation.innerText = showsInfo.location;
+  let showButton = document.createElement("button");
+  showButton.classList.add("shows__content--button");
+  showButton.innerText = "Buy Tickets";
+
+  showsEl.appendChild(h3Date);
+  showsEl.appendChild(pDate);
+  showsEl.appendChild(h3Venue);
+  showsEl.appendChild(pVenue);
+  showsEl.appendChild(h3Location);
+  showsEl.appendChild(pLocation);
+  showsEl.appendChild(showButton);
+  shows.appendChild(showsEl);
+}
+
+// let showInfo = [
+//   {
+//     date: "Mon Dec 17 2018",
+//     venue: "Ronald Lane",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Tue Jul 18 2019",
+//     venue: "Pier 3 East",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Fri Jul 22 2019",
+//     venue: "View Loungu",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Sat Aug 12 2019",
+//     venue: "Hyatt Agency",
+//     location: "San Fancisco, CA"
+//   },
+//   {
+//     date: "Fri Sep 05 2019",
+//     venue: "Moscow Center",
+//     location: "San Fancisco, CA"
+//   },
+//   { date: "Wed Aug 11 2019", venue: "Pres Club", location: "San Fancisco, CA" }
+// ];
+
+// function generateShowList(showInfoObj) {
+//   console.log(showInfoObj.date);
+//   let shows = document.querySelector(".shows");
+
+//   let showsEl = document.createElement("div");
+//   showsEl.classList.add("shows__content");
+
+//   let h3Date = document.createElement("h3");
+//   h3Date.classList.add("shows__content--date");
+//   h3Date.innerText = "DATE";
+//   let pDate = document.createElement("p");
+//   pDate.classList.add("shows__content--date--context");
+//   pDate.innerText = showInfoObj.date;
+//   let h3Venue = document.createElement("h3");
+//   h3Venue.classList.add("shows__content--venue");
+//   h3Venue.innerText = "VENUE";
+//   let pVenue = document.createElement("p");
+//   pVenue.classList.add("shows__content--venue--context");
+//   pVenue.innerText = showInfoObj.venue;
+//   let h3Location = document.createElement("h3");
+//   h3Location.classList.add("shows__content--location");
+//   h3Location.innerText = "LOCATION";
+//   let pLocation = document.createElement("p");
+//   pLocation.classList.add("shows__content--location--context");
+//   pLocation.innerText = showInfoObj.location;
+//   let showButton = document.createElement("button");
+//   showButton.classList.add("shows__content--button");
+//   showButton.innerText = "Buy Tickets";
+
+//   showsEl.appendChild(h3Date);
+//   showsEl.appendChild(pDate);
+//   showsEl.appendChild(h3Venue);
+//   showsEl.appendChild(pVenue);
+//   showsEl.appendChild(h3Location);
+//   showsEl.appendChild(pLocation);
+//   showsEl.appendChild(showButton);
+//   shows.appendChild(showsEl);
+// }
+// for (let i = 0; i < showInfo.length; i++) {
+//   generateShowList(showInfo[i]);
+// }
